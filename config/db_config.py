5523034cb35197dde19b8fef2c49645d7d9fcee5
juniorgutierrez05 <junior.gutierrezm05@gmail.com>
2025-11-17 20:28:38 -0500

    configuracion de conexion de db en la nube -CLEVER CLOUD-

--- config/db_config.py
+++ config/db_config.py
@@ -1,9 +1,10 @@
 from flask_mysqldb import MySQL
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''  # sin contraseña
-    app.config['MYSQL_DB'] = 'proyecto_ia'
+    app.config['MYSQL_HOST'] = 'b9qmehaxhy6yjls3pnt3-mysql.services.clever-cloud.com'
+    app.config['MYSQL_USER'] = 'unjbftdqf3uullb7'
+    app.config['MYSQL_PASSWORD'] = '7MRLPzHRMDHukCwYNgAE'
+    app.config['MYSQL_DB'] = 'b9qmehaxhy6yjls3pnt3'
+    app.config['MYSQL_PORT'] = 3306
     mysql = MySQL(app)
     return mysql
