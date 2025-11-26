f12daf63afa5632a115292893cd02b65454430af
MatthewsGomez <mattgomez3003@gmail.com>
2025-11-26 09:33:49 -0500

    credenciales db

--- config/db_config.py
+++ config/db_config.py
@@ -3,10 +3,10 @@ from flask_mail import Mail
 import os
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'b9qmehaxhy6yjls3pnt3-mysql.services.clever-cloud.com'
-    app.config['MYSQL_USER'] = 'unjbftdqf3uullb7'
-    app.config['MYSQL_PASSWORD'] = '7MRLPzHRMDHukCwYNgAE'
-    app.config['MYSQL_DB'] = 'b9qmehaxhy6yjls3pnt3'
+    app.config['MYSQL_HOST'] = 'bsiamiq4tnd2zfjvfe1e-mysql.services.clever-cloud.com'
+    app.config['MYSQL_USER'] = 'ux7dua5mflupv0ub'
+    app.config['MYSQL_PASSWORD'] = 'ux7dua5mflupv0ub'
+    app.config['MYSQL_DB'] = 'bsiamiq4tnd2zfjvfe1e'
     app.config['MYSQL_PORT'] = 3306
     mysql = MySQL(app)
     return mysql
