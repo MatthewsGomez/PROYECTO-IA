31bf4ee9a5215776d6a4712829fa916ef5ad6c41
MatthewsGomez <mattgomez3003@gmail.com>
2025-11-26 10:01:07 -0500

    act credenciales

--- config/db_config.py
+++ config/db_config.py
@@ -5,7 +5,7 @@ import os
 def init_db(app):
     app.config['MYSQL_HOST'] = 'bsiamiq4tnd2zfjvfe1e-mysql.services.clever-cloud.com'
     app.config['MYSQL_USER'] = 'ux7dua5mflupv0ub'
-    app.config['MYSQL_PASSWORD'] = 'ux7dua5mflupv0ub'
+    app.config['MYSQL_PASSWORD'] = 'qcnehmrZtnzplx7l4CZ8'
     app.config['MYSQL_DB'] = 'bsiamiq4tnd2zfjvfe1e'
     app.config['MYSQL_PORT'] = 3306
     mysql = MySQL(app)
